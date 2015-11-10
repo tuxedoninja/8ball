f3f1f0e8a160b95e1f4718e99fba10b49e033bc3
James Ballantyne <james.ballantyne@outlook.com>
2015-11-10 14:13:27 +0000

    8ball

---
+++ www/js/index.js
@@ -0,0 +1,19 @@
+$(document).on("pagecreate","#pageone",function(){
+  $('#submitButton').on("click", function(){
+    submitText();
+  });            
+});            
+
+
+function submitText() {
+	var text = $('#textinput').val();
+	alert(text);
+}
+
+function storeValue(key, value) {
+	//add some code to store the key-value pair in persistant storage 
+}
+
+function random() {
+  return !Math.round(Math.random());
+}
